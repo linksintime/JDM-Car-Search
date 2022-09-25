612326560462d9858b3b6df423422a3b2f5e85b3
linksintime <hellothereme12@gmail.com>
2022-09-25 00:02:58 -0400

    Made progress on web scraper, now outputs beautiful information in those boxes into lists

---
+++ cars.py
@@ -0,0 +1,8 @@
+class cars:
+    def __init__(self, make, model, year, VIN, mileage, cost):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.VIN = VIN
+        self.mileage = mileage
+        self.cost = cost
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys # Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,24 +7,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 import time
 
+# PARAMETERS
+search_key = "Nissan 180sx"
+
 PATH = "/home/asher/.local/bin/chromedriver"
-driver = webdriver.Chrome(PATH)
+options = Options()
+options.add_argument("--headless") # No web page is loaded. Doesn't have to open chromium
+driver = webdriver.Chrome(PATH ,options=options)
+list_of_cars = []
 
 driver.get("https://carfromjapan.com/")
 print(driver.title)
 
 search = driver.find_element("name", "keywords")
-search.send_keys("Toyota")
+search.send_keys(search_key)
 search.send_keys(Keys.RETURN)
 
-output = climage.convert('linkmotorcylcle.jpg', is_256color=True, is_unicode=True)
-print(output)
 
 try:
-    carList = WebDriverWait(driver, 10).until(
+    carTab = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "table-car"))
     )
-    carListText = carList.text
+    carRow = carTab.find_elements(By.CLASS_NAME, "car-row")
+
+    for rows in carRow: # Scrapes information about cars listings in car-row class and sorts it into a list
+        carlistings = list(rows.text.split("\n"))
+        list_of_cars.append(carlistings)
+
+    for cars in list_of_cars: # Beautiful view of those very lists
+        print(cars)
 
 finally:
 
